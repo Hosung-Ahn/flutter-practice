17b87fee6ee7205ba2f4c7f170d14127d041efc6
HOSUNG <an3735297@pusan.ac.kr>
2024-08-25 14:45:25 +0900

    custom Linear gradient - widget(child)를 파라미터로 받는 생성자

--- first_app/lib/home_page.dart
+++ first_app/lib/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_text.dart';
 
-const beginAlign = Alignment.topLeft;
-const Alignment endAlign = Alignment.bottomRight;
-
 // MyHomePage 위젯은 메인 화면을 나타냅니다.
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -14,25 +11,51 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Welcome to Flutter'),
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: beginAlign,
-            end: Alignment.bottomRight,
-            colors: [Colors.blue, Colors.green],
-          ),
-        ),
-        child: const Center(
+      body: const CustomLinearGradient(
+        colors: [
+          Colors.blue,
+          Colors.red,
+        ],
+        child: Center(
           child: CustomText(
-            text: 'Hello, Flutter!!',
+            text: 'Hello, World!',
             size: 30,
             color: Colors.white,
             weight: FontWeight.bold,
           ),
         ),
       ),
+    );
+  }
+}
+
+class CustomLinearGradient extends StatelessWidget {
+  final Alignment begin;
+  final Alignment end;
+  final List<Color> colors;
+  final Widget child;
+
+  const CustomLinearGradient({
+    this.begin = Alignment.topLeft,
+    this.end = Alignment.bottomRight,
+    required this.colors,
+    required this.child,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: begin,
+          end: end,
+          colors: colors,
+        ),
+      ),
+      child: child,
     );
   }
 }
